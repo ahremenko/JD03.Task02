0acdf21964c3198d0117ba0d221adf9d1e375b31
Alexey Ahremenko <a.ahremenko@gmail.com>
2020-07-20 21:31:59 +0300

    Задача выполнена и протестирована. Не было в условии, но определение анаграмм - регистронезависимо.

---
+++ src/main/java/by/htp/ahremenko/task21/service/AnagramService.java
@@ -0,0 +1,92 @@
+package by.htp.ahremenko.task21.service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 2.2 Анаграммы
+ * Анаграммы – это слова, которые состоят из одних и тех же символов, но в разном порядке.
+ * Дан массив строк, найти в нем анаграммы и вернуть список групп этих анаграмм.
+ * Каждый список отсортирован в алфавитном порядке. Оценить сложность написанного алгоритма.
+ * <p>
+ * Пример:
+ * <p>
+ * ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * <p>
+ * На выходе должны получить:
+ * <p>
+ * [
+ * <p>
+ * ["ate", "eat","tea"],
+ * <p>
+ * ["nat","tan"],
+ * <p>
+ * ["bat"]
+ * <p>
+ * ]
+ * <p>
+ * Примечание: каждая группа анаграмм тоже должна быть в нужном порядке,
+ * т.е. группа ["ate", "eat","tea"] идет первой, потому что слово “eat” было найдено первым
+ * в исходном массиве, ["nat","tan"] второй, потому что “tan” было найдено вторым
+ * (вообще третьим, но т.к. “tea” вошло в первую группу анаграмм, то считается вторым) и т.д.
+ */
+public final class AnagramService {
+
+    private AnagramService() {
+    }
+
+    public static List<List<String>> handle(List<String> list) {
+        List<List<String>> result = new ArrayList<>();
+
+        for (int i = 0; i < list.size(); i++) {
+            Integer found = null;
+            for (int j = 0; j < result.size(); j++) {
+                if (isAnagram(list.get(i), result.get(j).get(0))) {
+                    found = j;
+                    break;
+                }
+            }
+            if (found != null) {
+                result.get(found).add(list.get(i));
+            } else {
+                List<String> anagrams = new ArrayList<>();
+                anagrams.add(list.get(i));
+                result.add(anagrams);
+            }
+        }
+        result.forEach(a -> Collections.sort(a));
+        return result;
+    }
+
+    public static boolean isAnagram(String firstWord, String secondWord) {
+        char[] word1 = firstWord.toLowerCase().replaceAll("[\\s]", "").toCharArray();
+        char[] word2 = secondWord.toLowerCase().replaceAll("[\\s]", "").toCharArray();
+        Map<Character, Integer> lettersMap = new HashMap<>();
+
+        for (char c : word1) {
+            int count = 1;
+            if (lettersMap.containsKey(c)) {
+                count = lettersMap.get(c) + 1;
+            }
+            lettersMap.put(c, count);
+        }
+
+        for (char c : word2) {
+            int count = -1;
+            if (lettersMap.containsKey(c)) {
+                count = lettersMap.get(c) - 1;
+            }
+            lettersMap.put(c, count);
+        }
+
+        for (char c : lettersMap.keySet()) {
+            if (lettersMap.get(c) != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/test/by/htp/ahremenko/task21/service/AnagramServiceTest.java
@@ -0,0 +1,55 @@
+package by.htp.ahremenko.task21.service;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class AnagramServiceTest {
+
+    @Test
+    public void handle() {
+
+        List<String> list = new ArrayList<>();
+        list.add("eat");
+        list.add("tEa");
+        list.add("tan");
+        list.add("ate");
+        list.add("nat");
+        list.add("bat");
+        List<List<String>> actualList = AnagramService.handle(list);
+
+        List<List<String>> expectedList = new ArrayList<>();
+
+        List<String> list1 = new ArrayList<>();
+        list1.add("ate");
+        list1.add("eat");
+        list1.add("tEa");
+        expectedList.add(list1);
+
+        List<String> list2 = new ArrayList<>();
+        list2.add("nat");
+        list2.add("tan");
+        expectedList.add(list2);
+
+        List<String> list3 = new ArrayList<>();
+        list3.clear();
+        list3.add("bat");
+        expectedList.add(list3);
+
+        for (int i = 0; i < expectedList.size(); i++) {
+            String[] actual = new String[actualList.get(i).size()];
+            for (int j = 0; j < actualList.get(i).size(); j++) {
+                actual[j] = actualList.get(i).get(j);
+            }
+            String[] expected = new String[expectedList.get(i).size()];
+            for (int j = 0; j < expectedList.get(i).size(); j++) {
+                expected[j] = expectedList.get(i).get(j);
+            }
+            assertArrayEquals(expected, actual);
+        }
+    }
+
+}
